c272b9a29efad341c4bcfe899a802c062c115351
cham_why <53082717+chamwhy@users.noreply.github.com>
2026-07-26 05:23:45 +0000

    heal item 무한 버그 수정

--- godot/01_Scripts/Entities/Element/HealItem/HealItem.gd
+++ godot/01_Scripts/Entities/Element/HealItem/HealItem.gd
@@ -5,6 +5,7 @@ class_name HealItem
 #@export var is_block: bool = false  # 막힘 판정 여부
 #@export var hitable: bool = true
 var heal_pow := 1
+var healed := false
 #endregion
 
 
@@ -20,9 +21,9 @@ func apply_data(data: Dictionary) -> void:
 	# TODO: 상태에 따른 이미지 업데이트
 
 func _check_pos(pos: Position, elm: Element, tick: int) -> void:
-	if pos.equals(cur_position):
+	if not healed and pos.equals(cur_position):
 		heal(elm, tick)
-		destroy_heal_item(tick)
+		destroy_healItem(tick)
 
 func heal(target: Element, tick: int) -> void:
 	var new_heal_data = HealData.new(
@@ -32,6 +33,18 @@ func heal(target: Element, tick: int) -> void:
 	if is_instance_valid(target) and target.has_method("on_heal"):
 		target.on_heal(new_heal_data)
 
-func destroy_heal_item(tick: int) -> void:
-	GridManager.exit_element(self, cur_position, tick)
-	queue_free()
+
+func destroy_healItem(tick: int) -> void:
+	healed = true
+	hide_for_undo(tick) 
+
+func save_undo_state() -> Dictionary:
+	var dict = super.save_undo_state()
+	dict["healed"] = healed
+	return dict
+
+## save_undo_state()가 반환한 Dictionary로 논리 상태를 복원한다.
+## 서브클래스는 super.apply_undo(state)를 먼저 호출한 뒤 추가 처리한다.
+func apply_undo(state: Dictionary) -> void:
+	super.apply_undo(state)
+	healed = state.get("healed", false)
