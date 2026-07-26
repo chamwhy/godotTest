1741096e5bc06cc8769a42a33d8d330b1e48347a
cham_why <53082717+chamwhy@users.noreply.github.com>
2026-07-26 05:25:25 +0000

    debug: unit attack undo 애니메이션 무한 버그 수정

--- godot/01_Scripts/Entities/Element/Unit/Unit.gd
+++ godot/01_Scripts/Entities/Element/Unit/Unit.gd
@@ -231,6 +231,6 @@ func _anim_attack(tween: Tween, data: Dictionary) -> void:
 	tween.tween_interval(0.2)
 
 func _undo_anim_attack(tween: Tween, data: Dictionary) -> void:
-	pass
+	tween.tween_interval(0.0)
 
 #endregion
